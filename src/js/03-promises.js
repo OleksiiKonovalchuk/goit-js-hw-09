af571c5a33bbff187cdc3227bd47ad4765777b11
OleksiiKonovalchuk <108429819+OleksiiKonovalchuk@users.noreply.github.com>
2022-11-27 22:07:53 +0200

    DONE!

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  delay: document.querySelector("[name='delay']"),
+  step: document.querySelector("[name='step']"),
+  amount: document.querySelector("[name='amount']"),
+  form: document.querySelector('.form'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      return { position, delay };
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
+const promisePostponer = (delay, step, amount) => {
+  let newDelay = 0;
+  for (let i = 1; i <= Number(amount); i += 1) {
+    newDelay = i === 1 ? Number(delay) : (newDelay += Number(step));
+    createPromise(i, newDelay);
+  }
+};
+const onSubmit = e => {
+  e.preventDefault();
+  promisePostponer(refs.delay.value, refs.step.value, refs.amount.value);
+};
+refs.form.addEventListener('submit', onSubmit);
